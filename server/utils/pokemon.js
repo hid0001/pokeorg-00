fc4a9cd4ac37709158ea603eee27e7a72daa5a6e
hid <hidenori.funaki@konicaminolta.com>
2024-02-15 17:14:03 +0900

    03 apprunner用

--- server/utils/pokemon.js
+++ server/utils/pokemon.js
@@ -1,13 +1,6 @@
 /** ポケモンの取得 */
-import { ofetch } from "ofetch";
-import { ProxyAgent } from "proxy-agent";
-
-const agent = new ProxyAgent();
-
-/** ポケモンの取得 */
-export async function findPokemon(name) {
-  const pokemon = await ofetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
-    agent,
-  });
+export const findPokemon = async (name) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const pokemon = await response.json();
   return pokemon;
-}
+};
