a4ecb44f5f86f019ccede6b25f838a329383f4a2
hid <hidenori.funaki@konicaminolta.com>
2024-02-15 16:45:11 +0900

    02

--- server/utils/pokemon.js
+++ server/utils/pokemon.js
@@ -1,6 +1,13 @@
 /** ポケモンの取得 */
-export const findPokemon = async (name) => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-  const pokemon = await response.json();
+import { ofetch } from "ofetch";
+import { ProxyAgent } from "proxy-agent";
+
+const agent = new ProxyAgent();
+
+/** ポケモンの取得 */
+export async function findPokemon(name) {
+  const pokemon = await ofetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
+    agent,
+  });
   return pokemon;
-};
+}
